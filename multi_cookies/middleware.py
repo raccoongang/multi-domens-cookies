c084a4c659b07e6800d0ac3844bf5b974bb1c81f
dorosh <dorosshh@gmail.com>
2016-09-27 14:17:38 +0300

    add middleware

---
+++ multi_cookies/middleware.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+
+
+class UserStandingMiddleware(object):
+    """
+    Checks a user's standing on request. Returns a 403 if the user's
+    status is 'disabled'.
+    """
+    def process_response(self, request, response):
+        user = request.user
+        is_auth = user.is_authenticated()
+
+        response.set_cookie('authenticated', str(int(is_auth)),
+                            domain=settings.AUTH_SESSION_COOKIE_DOMAIN,
+                            secure=settings.SESSION_COOKIE_SECURE or None,
+                            max_age=settings.SESSION_COOKIE_AGE)
+        response.set_cookie('authenticated_user',
+                            is_auth and user.username or 'Anonymous',
+                            domain=settings.AUTH_SESSION_COOKIE_DOMAIN,
+                            secure=settings.SESSION_COOKIE_SECURE or None,
+                            max_age=settings.SESSION_COOKIE_AGE)
+        return response
