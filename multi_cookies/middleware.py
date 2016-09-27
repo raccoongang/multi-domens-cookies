cc13c614266056b918708c7074902a969dfe7607
dorosh <dorosshh@gmail.com>
2016-09-27 15:30:30 +0300

    add middleware

--- multi_cookies/middleware.py
+++ multi_cookies/middleware.py
@@ -1,5 +1,8 @@
+import logging
+
 from django.conf import settings
 
+log = logging.getLogger(__name__)
 
 class UserStandingMiddleware(object):
     """
@@ -7,16 +10,18 @@ class UserStandingMiddleware(object):
     status is 'disabled'.
     """
     def process_response(self, request, response):
-        user = request.user
-        is_auth = user.is_authenticated()
+        log.info('{} !!!!'.format(dir(request)))
+        if 'admin' in request.path:
+            user = request.user
+            is_auth = user.is_authenticated()
 
-        response.set_cookie('authenticated', str(int(is_auth)),
-                            domain=settings.AUTH_SESSION_COOKIE_DOMAIN,
-                            secure=settings.SESSION_COOKIE_SECURE or None,
-                            max_age=settings.SESSION_COOKIE_AGE)
-        response.set_cookie('authenticated_user',
-                            is_auth and user.username or 'Anonymous',
-                            domain=settings.AUTH_SESSION_COOKIE_DOMAIN,
-                            secure=settings.SESSION_COOKIE_SECURE or None,
-                            max_age=settings.SESSION_COOKIE_AGE)
+            response.set_cookie('authenticated', str(int(is_auth)),
+                                domain=settings.AUTH_SESSION_COOKIE_DOMAIN,
+                                secure=settings.SESSION_COOKIE_SECURE or None,
+                                max_age=settings.SESSION_COOKIE_AGE)
+            response.set_cookie('authenticated_user',
+                                is_auth and user.username or 'Anonymous',
+                                domain=settings.AUTH_SESSION_COOKIE_DOMAIN,
+                                secure=settings.SESSION_COOKIE_SECURE or None,
+                                max_age=settings.SESSION_COOKIE_AGE)
         return response
